1524d126ae398efb85781d5e80e911d6dadc953c
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2022-01-21 09:38:51 +0000

    fix qr

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,19 @@
 import React from "react";
-import { useRouter } from "next/router";
 import QRCode from "react-qr-code";
 import { Container, Box, Heading, HStack } from "@chakra-ui/react";
 
+const origin = "http://localhost:3000";
 export default function Home() {
-  const router = useRouter();
-  console.log(router);
-
   return (
     <Container>
       <HStack spacing={32}>
         <Box>
           <Heading size="md">Claim your NFT</Heading>
-          <QRCode value={`/claim`} size={256} />
+          <QRCode value={`${origin}/claim`} size={256} />
         </Box>
         <Box>
           <Heading size="md">Enter Member Zone</Heading>
-          <QRCode value={`/member`} size={256} />
+          <QRCode value={`${origin}/member`} size={256} />
         </Box>
       </HStack>
     </Container>
